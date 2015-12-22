dc0faa556619131abdfe9c7b386b542ccc118ba9
ripnraks300 <ErickTruong@macbook-pro.home>
2015-12-21 23:11:14 -0500

    Initial Commit

---
+++ retroCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  retroCalc
+//
+//  Created by ERICK TRUONG on 12/21/15.
+//  Copyright © 2015 Code Create Innovate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
